1357b596154a7519a21173dfa87357dd5a4d1454
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-16 22:25:26 +0200

    refactor module

--- lib/roman_numerals.ex
+++ lib/roman_numerals.ex
@@ -1,18 +1,54 @@
 defmodule RomanNumerals do
-  @moduledoc """
-  Documentation for `RomanNumerals`.
-  """
+  @roman_m %Roman{symbol: "M", value: 1000}
+  @roman_d %Roman{symbol: "D", value: 500}
+  @roman_c %Roman{symbol: "C", value: 100}
+  @roman_l %Roman{symbol: "L", value: 50}
+  @roman_x %Roman{symbol: "X", value: 10}
+  @roman_v %Roman{symbol: "V", value: 5}
+  @roman_i %Roman{symbol: "I", value: 1}
+  @roman_none %Roman{symbol: "", value: 0}
 
-  @doc """
-  Hello world.
+  defp get_next(@roman_m), do: @roman_d
+  defp get_next(@roman_d), do: @roman_c
+  defp get_next(@roman_c), do: @roman_l
+  defp get_next(@roman_l), do: @roman_x
+  defp get_next(@roman_x), do: @roman_v
+  defp get_next(@roman_v), do: @roman_i
+  defp get_next(@roman_i), do: @roman_none
 
-  ## Examples
+  defp get_restable(@roman_m), do: @roman_c
+  defp get_restable(@roman_d), do: @roman_c
+  defp get_restable(@roman_c), do: @roman_x
+  defp get_restable(@roman_l), do: @roman_x
+  defp get_restable(@roman_x), do: @roman_i
+  defp get_restable(@roman_v), do: @roman_i
+  defp get_restable(@roman_i), do: @roman_none
 
-      iex> RomanNumerals.hello()
-      :world
+  def from_arabic_to_roman_string(number) do
+    from_arabic(number) |> Enum.map(fn r -> r.symbol end) |> Enum.join()
+  end
 
-  """
-  def hello do
-    :world
+  defp from_arabic(number) do
+    number |> from_arabic(@roman_m)
+  end
+
+
+  defp from_arabic(number, @roman_i) do
+    @roman_i |> List.duplicate(number)
+  end
+
+  defp from_arabic(number, roman) when number >= roman.value do
+    [roman | from_arabic(number - roman.value)]
+  end
+
+  defp from_arabic(number, roman) do
+    restable = get_restable(roman)
+    should_have_previous_unit = number >= (roman.value - restable.value)
+
+    if should_have_previous_unit do
+      [restable, roman | from_arabic(number - (roman.value - restable.value))]
+    else
+      from_arabic(number, get_next(roman))
+    end
   end
 end
--- test/roman_numerals_test.exs
+++ test/roman_numerals_test.exs
@@ -2,7 +2,24 @@ defmodule RomanNumeralsTest do
   use ExUnit.Case
   doctest RomanNumerals
 
-  test "greets the world" do
-    assert RomanNumerals.hello() == :world
+  @parameters "./romans.csv"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.drop(1)
+    |> Stream.map(&String.trim(&1, "\n"))
+    |> Stream.map(&String.split(&1, ","))
+    |> Stream.map(fn ([arabic, roman]) -> {arabic |> String.to_integer, roman} end)
+    |> Enum.to_list
+
+  # for {input, expected} <- @parameters do
+  #   test "from_arabic(#{input}) == #{expected}" do
+  #     assert RomanValue.from_arabic_to_roman_string(unquote(input)) == unquote(expected)
+  #   end
+  # end
+
+  test "it works" do
+    for {input, expected} <- @parameters do
+      assert RomanNumerals.from_arabic_to_roman_string(input) == expected
+    end
   end
 end
